3069f8fb54eae50e62c1c4e8ed15eb388aa3c573
Maria22 <m.ushakova22@gmail.com>
2021-04-10 19:54:00 +0200

    -add navLink

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Nav.module.css'
 
 const Nav = () => {
     return (
         <ul className={style.appNav}>
-            <li className={style.active}>
-                <a href='/profile'>Home</a>
+            <li>
+                <NavLink to='/profile' activeClassName={style.activeLink}>Home</NavLink>
             </li>
             <li>
-                <a href='/dialog'>Messages</a>
+                <NavLink to='/dialog' activeClassName={style.activeLink}>Messages</NavLink>
             </li>
             <li>
-                <a href='/news'>News</a>
+                <NavLink to='/news' activeClassName={style.activeLink}>News</NavLink>
             </li>
             <li>
-                <a href='/music'>Music</a>
+                <NavLink to='/music' activeClassName={style.activeLink}>Music</NavLink>
             </li>
             <li>
-                <a href='/settings'>Settings</a>
+                <NavLink to='/settings' activeClassName={style.activeLink}>Settings</NavLink>
             </li>
         </ul >
     );
