ca63cd5cbac4141ea94b0ab354c66109fe12cc5e
Maria22 <m.ushakova22@gmail.com>
2021-04-10 20:15:45 +0200

    -add dialogs information

--- src/components/Dialogs/Dialog.jsx
+++ src/components/Dialogs/Dialog.jsx
@@ -4,8 +4,19 @@ import style from './Dialog.module.css'
 
 const Dialog = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <ul className={style.dialog}>
+                <li>Mariia</li>
+                <li>Anna</li>
+                <li>Alina</li>
+                <li>Malina</li>
+                <li>Eva</li>
+            </ul>
+            <ul className={style.message}>
+                <li>Hi</li>
+                <li>How are you!</li>
+                <li>I am OK</li>
+            </ul>
         </div>
     );
 }
