60c3134db0d2e5258c1a28c503afa01304014397
Maria22 <m.ushakova22@gmail.com>
2021-04-15 20:55:24 +0200

    rename dialogs

--- src/components/Dialogs/DialogList.jsx
+++ src/components/Dialogs/DialogList.jsx
@@ -4,13 +4,13 @@ import MessageItem from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
 
 const DialogList = (props) => {
-    let messageItem = props.messages.map(message => <MessageItem message={message.message} />);
-    let dialogElement = props.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+    const messageItem = props.messages.map(message => <MessageItem message={message.message} />);
+    const dialogItem = props.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
 
     return (
         <div className={style.dialogs}>
             <ul className={style.dialog}>
-                {dialogElement}
+                {dialogItem}
             </ul>
             <ul className={style.message}>
                 {messageItem}
