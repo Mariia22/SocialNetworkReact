4673ee23cfba0431f3faa25f5c887baef6b31253
Maria22 <m.ushakova22@gmail.com>
2021-09-15 15:30:17 +0200

    add user photo from file for users without photos

--- src/components/Profile/ProfileFriend.jsx
+++ src/components/Profile/ProfileFriend.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './Profile.module.css'
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import userPhoto from './../../images/user.png';
 
 const ProfileFriend = (props) => {
     return (
         <div className={style.appProfile}>
             <div className={style.appProfileRed}></div>
-            <img className={style.appProfilePhoto} src={props.profile.photos.small} alt="User pic" width="100px" height="100px" />
+            <img className={style.appProfilePhoto} src={props.profile.photos.large || userPhoto} alt="User pic" width="100px" height="100px" />
             <div className={style.appProfileWhite}>
                 <h3>{props.profile.fullName}</h3>
                 <p>{props.profile.aboutMe}</p>
