d3a8c430e224cad1c103f39f8fdb1d5aea7c0ec1
Maria22 <m.ushakova22@gmail.com>
2021-09-15 11:00:50 +0200

    add pagination buttons prev-next

--- src/components/Friend/FriendList.jsx
+++ src/components/Friend/FriendList.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import style from './Friend.module.css';
 import Friend from './Friend';
+import Pagination from '../Common/Pagination';
 
 const FriendList = (props) => {
-    let count = Math.ceil(props.totalCount / props.pageSize);
-    let pagination = [];
-    for (let i = 1; i <= count; i++) {
-        pagination.push(i)
-    }
     const changeToggle = (id, follow) => {
         props.setIsFetching(true, id);
         follow ? props.unfollowUser(id) : props.followUser(id);
     }
-    return <div>
-        <div className={style.paginationList}>
-            {
-                pagination.map(page => <span className={props.currentPage === page ? style.selected : style.nonselected}
-                    onClick={() => { props.changeCurrentPage(page) }}>{page}</span>)
-            }
+    return (
+        <div>
+            <Pagination totalCount={props.totalCount} pageSize={props.pageSize} changeCurrentPage={props.changeCurrentPage} currentPage={props.currentPage} />
+            <div className={style.friendList} >
+                {
+                    props.users.map(user => <Friend key={user.id} id={user.id} photo={user.photos.small} name={user.name}
+                        followed={user.followed} isFetching={props.isFetching} changeToggle={changeToggle} />)
+                }
+            </div>
         </div>
-        <div className={style.friendList} >
-            {
-                props.users.map(user => <Friend key={user.id} id={user.id} photo={user.photos.small} name={user.name}
-                    followed={user.followed} isFetching={props.isFetching} changeToggle={changeToggle} />)
-            }
-        </div>
-    </div>
+    )
 };
 
 export default FriendList;
