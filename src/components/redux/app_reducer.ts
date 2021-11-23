0c604262228655e58154f87bd6821b09e1dc2577
Maria22 <m.ushakova22@gmail.com>
2021-11-23 12:10:06 +0100

    add typescript to the project

--- src/components/redux/app_reducer.ts
+++ src/components/redux/app_reducer.ts
@@ -3,25 +3,25 @@ import { getAuth } from "./login_reduce";
 const SET_INITIALIZED = 'SET_INITIALIZED';
 
 let initialState = {
-    initialized: false
+  initialized: false
 }
 const appReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_INITIALIZED: {
-            return {
-                ...state,
-                initialized
-            }
-        }
-        default: { return state }
+  switch (action.type) {
+    case SET_INITIALIZED: {
+      return {
+        ...state,
+        initialized
+      }
     }
+    default: { return state }
+  }
 }
 
 const initializedSuccseed = (initialized) => ({ type: SET_INITIALIZED, initialized });
 export const initialized = () => (dispatch) => {
-    let promise = dispatch(getAuth);
-    promise.then(() => {
-        dispatch(initializedSuccseed(true))
-    })
+  let promise = dispatch(getAuth);
+  promise.then(() => {
+    dispatch(initializedSuccseed(true))
+  })
 }
 export default appReducer;
